36b006a887b917f495b6b6df2977bc8470968bd8
janpauldahlke <janpauldahlke@gmail.com>
2018-02-12 09:45:56 +0100

    how to update snapshots

--- giftgiver/src/components/App.js
+++ giftgiver/src/components/App.js
@@ -3,7 +3,12 @@ import React, { Component } from 'react';
 class App extends Component{
 
     render() {
-        return (<div></div>)
+        return (<div>
+            {/* adding this will make the test fail
+                use runner jest-cli do update failing snapshots
+            */}
+            <h2>gift giver</h2>
+        </div>)
     }
 }
 
