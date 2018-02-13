fdca824a64bd12c75661bfef54963b5e7cfae9bd
janpauldahlke <janpauldahlke@gmail.com>
2018-02-13 06:57:55 +0100

    grouping tests in a BDD manner in describe block

--- giftgiver/src/components/App.test.js
+++ giftgiver/src/components/App.test.js
@@ -3,34 +3,37 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App.js'; // this not exists yet // the test should fail
 
-// create jsx component app as shallow renderer
-const app = shallow(<App />);
 
-//unit test
-//checks that component renders correct
+describe('App', () => {
+    // create jsx component app as shallow renderer
+    const app = shallow(<App />);
 
-it('renders App correctly', () => {
-    expect(app).toMatchSnapshot();
-});
+    //unit test
+    //checks that component renders correct
 
-it('initializes the `state` with an empty list of gifts', () => {
-    //app.state to reach for state
-    expect(app.state().gifts).toEqual([]);
-});
+    it('renders App correctly', () => {
+        expect(app).toMatchSnapshot();
+    });
 
-it('adds a new gift to `state` when clicking `add gift` button', () => {
-    //find the button
-    app.find('.btn-add').simulate('click');
-    //expect results
-    expect(app.state().gifts).toEqual([{id: 1}])
-});
+    it('initializes the `state` with an empty list of gifts', () => {
+        //app.state to reach for state
+        expect(app.state().gifts).toEqual([]);
+    });
 
-it('adds a new gift the the rendered list, when clcking the `add gift` button', () => {
-    //carefully here, the button gets clicked twice, so the 
-    //element in the test is already at 2 eleemts
-    // -> its a test pollution
-    // make this test an isolated instance
+    it('adds a new gift to `state` when clicking `add gift` button', () => {
+        //find the button
+        app.find('.btn-add').simulate('click');
+        //expect results
+        expect(app.state().gifts).toEqual([{id: 1}])
+    });
 
-    app.find('.btn-add').simulate('click');
-    expect(app.find('.gift-list').children().length).toEqual(2);
+    it('adds a new gift the the rendered list, when clcking the `add gift` button', () => {
+        //carefully here, the button gets clicked twice, so the 
+        //element in the test is already at 2 eleemts
+        // -> its a test pollution
+        // make this test an isolated instance
+
+        app.find('.btn-add').simulate('click');
+        expect(app.find('.gift-list').children().length).toEqual(2);
+    })
 })
