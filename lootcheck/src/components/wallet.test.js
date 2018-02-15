d734d1fb574b05cc6c020349e1bdeb224fc0675f
janpauldahlke <janpauldahlke@gmail.com>
2018-02-15 09:05:21 +0100

    successfully test props on wallet

--- lootcheck/src/components/wallet.test.js
+++ lootcheck/src/components/wallet.test.js
@@ -4,14 +4,18 @@ import { shallow } from 'enzyme';
 import { Wallet } from './Wallet';
 
 describe('Wallet', () => {
+    const balance = 20
+    const props = {balance: balance}
 
-    
-    const wallet = shallow(<Wallet />)
+    const wallet = shallow(<Wallet {...props}/>)
 
     it('renders correctly', () => {
         expect(wallet).toMatchSnapshot();
     })
 
-
-
+    it('display balance from props', () => {
+        //this is not working, we need to DOM select the balance!!
+        //expect(wallet.props().balance).toEqual(balance);
+        expect(wallet.find('.balance').text()).toEqual('Wallet Balance: 20');
+    })
 })
